a4f1564007b228e2a0cfc1ff994b56c058c0c5bf
SSJ7AngeloJose <angelo7ssj@gmail.com>
2025-08-21 11:52:25 -0500

    actualizacion 2025

---
+++ src/hello/hola.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class hola {
+    public static void main(String[] args){
+        int age = 12;
+        String name = "angelo";
+        String ciudad = "";
+        double salary = 1100;
+        boolean sexo = true;
+
+        System.out.println(age + name +  ciudad +    salary +   sexo);
+    }
+}
