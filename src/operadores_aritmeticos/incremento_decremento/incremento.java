424c62607f0bff8fa9ed97a8be180bf3833e3ebe
SSJ7AngeloJose <angelo7ssj@gmail.com>
2025-08-23 16:09:29 -0500

    icremento

---
+++ src/operadores_aritmeticos/incremento_decremento/incremento.java
@@ -0,0 +1,9 @@
+package operadores_aritmeticos.incremento_decremento;
+
+public class incremento {
+    public static void main(String[] args) {
+        int x = 5;
+        x++;
+        System.out.println(x);
+    }
+}
