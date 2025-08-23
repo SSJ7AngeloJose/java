7701506006f60f4de3806eab9355254a46c0e227
SSJ7AngeloJose <angelo7ssj@gmail.com>
2025-08-23 16:14:57 -0500

    icremento

---
+++ src/operadores_aritmeticos/incremento_decremento/variables.java
@@ -0,0 +1,11 @@
+package operadores_aritmeticos.incremento_decremento;
+
+public class variables {
+    public static void main(String[] args) {
+        int x = 4, y;
+        //subfijo   //prefijo  = y = ++x;
+        y = x++;
+
+        System.out.println(y);
+    }
+}
