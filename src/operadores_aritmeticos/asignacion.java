fc2dc7a1756edeb729914c34fea720211684f582
SSJ7AngeloJose <angelo7ssj@gmail.com>
2025-08-23 16:04:40 -0500

    tramas

--- src/operadores_aritmeticos/asignacion.java
+++ src/operadores_aritmeticos/asignacion.java
@@ -1,4 +1,15 @@
 package operadores_aritmeticos;
 
 public class asignacion {
+    public static void main(String[] args){
+        int num1 = 10;
+        num1  += 6; //numero = numero +  5;
+        num1  -= 4;
+        num1  *= 5;
+        num1  /= 7;
+        num1  %= 6;
+
+        System.out.println(num1);
+    }
+
 }
