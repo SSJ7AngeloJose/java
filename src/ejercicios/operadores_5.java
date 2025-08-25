9a25f44e832ebdb4279676b278df144b3b82f174
SSJ7AngeloJose <angelo7ssj@gmail.com>
2025-08-25 14:26:24 -0500

    actualizado 25-08-25

---
+++ src/ejercicios/operadores_5.java
@@ -0,0 +1,31 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class operadores_5 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        float participacion , califacion1 , calificacion2 , examenfinal , notafinal;
+
+        System.out.print("ingrese la nota de participacion del estudiante : ");
+        participacion = entrada.nextFloat();
+        System.out.print("ingrese la 1 nota parcial  del estudiante : ");
+        califacion1 = entrada.nextFloat();
+        System.out.print("ingrese la 2 nota parcial  del estudiante : ");
+        calificacion2 = entrada.nextFloat();
+        System.out.print("ingrese la nota del exame final: ");
+        examenfinal = entrada.nextFloat();
+
+
+        participacion *= 0.10f;  califacion1 *= 0.25f; calificacion2 *= 0.25f; examenfinal *= 0.40f;
+
+        notafinal = participacion + califacion1 + calificacion2 + examenfinal ;
+
+
+        System.out.println("la nota final del estudiante es: " + notafinal );
+
+
+
+
+    }
+}
