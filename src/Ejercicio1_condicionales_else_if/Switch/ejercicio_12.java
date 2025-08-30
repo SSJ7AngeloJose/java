f6d48f84afeadf777bcf31e0ca0bb09820947c64
SSJ7AngeloJose <angelo7ssj@gmail.com>
2025-08-30 16:05:26 -0500

    la2025

---
+++ src/Ejercicio1_condicionales_else_if/Switch/ejercicio_12.java
@@ -0,0 +1,54 @@
+package Ejercicio1_condicionales_else_if.Switch;
+
+import javax.swing.*;
+
+
+/*/*Suficiente,
+Notable y Sobresaliente.
+
+        Insuficiente,
+
+        · Ejercicio 12: Pedir una nota de 0
+a 10 y mostrarla de la forma:
+Bien,
+*/
+
+public class ejercicio_12 {
+    public static void main(String[] args){
+        int nota = 0 - 10;
+
+        nota = Integer.parseInt(JOptionPane.showInputDialog("digite una nota de 0 a 10: "));
+
+        switch(nota){
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            JOptionPane.showMessageDialog(null, "insuficiente" + nota);
+            break;
+            case 4:
+                JOptionPane.showMessageDialog(null, "suficiente" + nota);
+                break;
+            case 5:
+                JOptionPane.showMessageDialog(null, "bien" + nota);
+                break;
+            case 6:
+                JOptionPane.showMessageDialog(null,"Notable" + nota);
+                break;
+            case 7:
+            case 8:
+                JOptionPane.showMessageDialog(null, "Sobresaliente" + nota);
+                break;
+            case 9:
+            case 10:
+                JOptionPane.showMessageDialog(null, "Matricula de honor" + nota);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null,"no existe la nota ");
+        }
+
+
+
+
+    }
+}
