ccd35e4d155f8db10a7b00ecc9d21e8a3792ca23
SSJ7AngeloJose <angelo7ssj@gmail.com>
2025-09-17 17:24:03 -0500

    update

--- src/arreglos/ejercicios/ejercicio_3.java
+++ src/arreglos/ejercicios/ejercicio_3.java
@@ -1,7 +1,55 @@
 package arreglos.ejercicios;
 
+import java.util.Scanner;
+
+/// ejercicios 3 leer 5 numeros por teclado , almacenarlos en un arreglo y a continuacion realizar la media de los
+/// numeros positivos , la media de los negativos y contar el numero de ceros.
+///
 public class ejercicio_3 {
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        float numeros[] = new float[5];
+        float suma_positivos=0, suma_negativos=0, media_positivos, media_negativos;
+        int conteo_positivos=0, conteo_negativos=0, conteo_ceros=0;
+
+        System.out.println("almacenando los datos");
+
+        for (int i=0; i < 5; i++){
+            System.out.println((i+1)+"digite un numero: ");
+            numeros[i] = sc.nextFloat();
+
+            if (numeros[i] == 0){
+                conteo_ceros++;
+            } else if (numeros[i] > 0) {
+                suma_positivos += numeros[i];
+                conteo_positivos++;
+            }
+            else{
+                suma_negativos += numeros[i];
+                conteo_negativos++;
+            }
+
+        }
+
+        //media de los numeros positivos
+        if (conteo_positivos == 0){
+            System.out.println("no se puede sacar la media de los numeros positivos");
+        }
+        else{
+            media_positivos = suma_positivos / conteo_positivos;
+            System.out.println("la media de los numeros positivos es: "+ media_positivos);
+        }
+        //media numeros negativos
+        if (conteo_negativos == 0){
+            System.out.println("no se puede sacar la media de los numeros negativos");
+        }
+        else {
+            media_negativos = suma_negativos / conteo_negativos;
+            System.out.println("la media de los numeros negativos es " + media_negativos);
+        }
+
+        System.out.println("la cantidad de ceros es :" + conteo_ceros);
 
     }
 }
--- src/arreglos/ejercicios/ejercicio_4.java
+++ src/arreglos/ejercicios/ejercicio_4.java
@@ -1,4 +1,36 @@
 package arreglos.ejercicios;
 
+import java.util.Scanner;
+
+//leer 10 numeros enteros , guardarlos en un arreglo, debemos mostrarlos en el siguiente orden : el primero , el
+// ultimo ,  el segundo , el penultimo , el tercero .
 public class ejercicio_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[10];
+        int inicio = 0;
+        int fin = numeros.length - 1;
+
+
+        System.out.println("guardando arreglos");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println((i + 1) + " 1digite los numeros: ");
+            numeros[i] = sc.nextInt();
+        }
+
+        System.out.println("Mostrando en orden: primero, último, segundo, penúltimo...");
+        while (inicio <= fin) {
+            if (inicio == fin) {
+                System.out.println(numeros[inicio]);
+            } else {
+                System.out.println(numeros[inicio]);
+                System.out.println(numeros[fin]);
+            }
+            inicio++;
+            fin--;
+
+        }
+    }
+
 }
